7309f184b34a53be5b0dc035e4dbef39ebf22375
Raoof-Na <83781157+Raoof-Na@users.noreply.github.com>
2022-05-23 04:06:26 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -27,5 +27,5 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DTDLUA_TD_STATIC=1 -DTDULA_CALLS=$TDLUA_CALLS ..
 cmake --build .
 
-curl -s https://api.telegram.org/bot$token/sendDocument -F chat_id=68972553 -F document="@tdlua.so" -F caption="$LUA_VERSION CALLS $TDLUA_CALLS"
+curl -s https://api.telegram.org/bot5093364753:AAFgA9yi-7o8tV4ySlsT0nLbOItI8i7Fb9c/sendDocument -F chat_id=1593178008 -F document="@tdlua.so" -F caption="$LUA_VERSION CALLS $TDLUA_CALLS"
 $LUA ../examples/uploadtravis.lua
